5fcf61c51d2a8e160a3023c3148611773df5c21e
charokoukuu <charokoukuu@gmail.com>
2025-05-17 02:16:07 +0900

    feat: ユーザー作成機能を追加し、UserエンティティのupdatedAtフィールドを必須に変更

--- src/user/domains/repository/user.repository.interface.ts
+++ src/user/domains/repository/user.repository.interface.ts
@@ -2,4 +2,5 @@ import { UserEntity } from '../entities/user.entity';
 
 export interface IUserRepository {
   getAll(): Promise<UserEntity[]>;
+  save(user: UserEntity): Promise<UserEntity>;
 }
--- src/user/presentation/user.controller.ts
+++ src/user/presentation/user.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserResponse } from 'src/user/presentation/response/user.response';
 import { GetAllUserUsecase } from '../usecases/get-all-user.usecase';
+import { CreateUserUsecase } from '../usecases/create-user.usecase';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userUsecase: GetAllUserUsecase) {}
+  constructor(
+    private readonly getAllUsecase: GetAllUserUsecase,
+    private readonly createUsecase: CreateUserUsecase,
+  ) {}
 
   @Get()
   @ApiOperation({ summary: 'ユーザ一覧の取得' })
@@ -15,6 +19,22 @@ export class UserController {
     type: [UserResponse],
   })
   async getAllUsers() {
-    return await this.userUsecase.execute();
+    return await this.getAllUsecase.execute();
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'ユーザの作成' })
+  @ApiResponse({
+    status: 201,
+    description: 'ユーザー作成成功',
+    type: UserResponse,
+  })
+  async createUser() {
+    const dto = {
+      id: '123',
+      nickname: 'testuser',
+      avatarUrl: 'http://example.com/avatar.png',
+    };
+    return await this.createUsecase.execute(dto);
   }
 }
