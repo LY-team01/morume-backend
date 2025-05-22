1c991e0841dc9b4bb89c5f85b5f77ffa2702180d
Hinata Nakano <0427practice@gmail.com>
2025-05-23 02:27:48 +0900

    dbスキーマにGroupを追加した。さらにseed.tsでグループを複数作成し、ユーザーがランダムにそれぞれのグループに振られるようにした。

---
+++ src/group/domains/entities/group.entity.ts
@@ -0,0 +1,63 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from 'typeorm';
+  
+  @Entity('groups')
+  export class GroupEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column({ type: 'varchar', length: 100, nullable: false })
+    name: string;
+  
+    @Column({ type: 'varchar', length: 100, nullable: true })
+    avatarUrl: string | null;
+  
+    @Column({ type: 'json', nullable: true })
+    filter: Record<string, any>;
+  
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+  
+    @UpdateDateColumn({ type: 'timestamp', nullable: false })
+    updatedAt: Date;
+  
+    static create(data: {
+      id: string;
+      nickname: string;
+      avatarUrl: string | null;
+      filter: Record<string, any>;
+    }): UserEntity {
+      const user = new UserEntity();
+      user.id = data.id;
+      user.nickname = data.nickname;
+      user.avatarUrl = data.avatarUrl;
+      user.filter = data.filter;
+      user.createdAt = new Date();
+      user.updatedAt = new Date();
+      return user;
+    }
+  
+    static factory(data: {
+      id: string;
+      nickname: string;
+      avatarUrl: string;
+      filter: Record<string, any>;
+      createdAt: Date;
+      updatedAt: Date | null;
+    }): UserEntity {
+      const user = new UserEntity();
+      user.id = data.id;
+      user.nickname = data.nickname;
+      user.avatarUrl = data.avatarUrl;
+      user.filter = data.filter;
+      user.createdAt = data.createdAt;
+      user.updatedAt = data.updatedAt;
+      return user;
+    }
+  }
+  
